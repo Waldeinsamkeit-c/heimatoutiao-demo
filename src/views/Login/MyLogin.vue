bf071c0bf9768c01bbaa08b6d1a53f7f5556ce1f
Waldeinsamkeit-c <965832962@qq.com>
2022-07-05 23:03:04 +0800

    000

--- src/views/Login/MyLogin.vue
+++ src/views/Login/MyLogin.vue
@@ -1,47 +1,9 @@
 <template>
-  <div>
-    <van-nav-bar title="标题" />
-    <van-form @submit="onSubmit">
-      <van-field
-        left-icon="shouji"
-        v-model="username"
-        name="用户名"
-        label="用户名"
-        placeholder="用户名"
-        :rules="[{ required: true, message: '请填写用户名' }]"
-      />
-      <template>
-        <IConFont class="left-icon shouji" />
-      </template>
-      <van-field
-        v-model="password"
-        type="password"
-        name="密码"
-        label="密码"
-        placeholder="密码"
-        :rules="[{ required: true, message: '请填写密码' }]"
-      />
-      <div style="margin: 16px">
-        <van-button block type="info" native-type="submit">提交</van-button>
-      </div>
-    </van-form>
-  </div>
+  <div></div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      username: "",
-      password: "",
-    };
-  },
-  methods: {
-    onSubmit(values) {
-      console.log("submit", values);
-    },
-  },
-};
+export default {};
 </script>
 
 <style scoped lang="less"></style>
