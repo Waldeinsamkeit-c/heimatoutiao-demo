07c5367500fda382c20d15d28d68cd3699cc72bb
Waldeinsamkeit-c <965832962@qq.com>
2022-07-05 23:15:49 +0800

    00000

--- src/views/Login/MyLogin.vue
+++ src/views/Login/MyLogin.vue
@@ -1,9 +1,70 @@
 <template>
-  <div></div>
+  <div>
+    <van-nav-bar title="登录" />
+    <van-form @submit="onSubmit">
+      <van-field
+        required
+        v-model="username"
+        name="用户名"
+        placeholder="用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      >
+        <template #left-icon>
+          <IconFont icon="shouji"></IconFont>
+        </template>
+      </van-field>
+      <van-field
+        required
+        v-model="password"
+        type="password"
+        name="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      >
+        <template #left-icon> <IconFont icon="yanzhengma"></IconFont> </template
+      ></van-field>
+      <div style="margin: 16px">
+        <van-button block type="info" native-type="submit">提交</van-button>
+      </div>
+    </van-form>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    onSubmit(values) {
+      console.log("submit", values);
+    },
+  },
+};
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.login-container {
+  .toutiao {
+    font-size: 60px;
+  }
+  .send-sms-btn {
+    width: 152px;
+    height: 46px;
+    line-height: 46px;
+    background-color: #ededed;
+    font-size: 22px;
+    color: #666;
+  }
+  .login-btn-wrap {
+    padding: 53px 33px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+    }
+  }
+}
+</style>
